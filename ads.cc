1053fbb93cba86c0c13b667c021d8cfa43c09e1a
Mera007 <39528120+Mera007@users.noreply.github.com>
2018-05-22 23:50:51 +0800

    Add files via upload

---
+++ ads.cc
@@ -0,0 +1,13 @@
+[{
+"title": "Facebook",
+"pkn": "com.facebook.katana",
+"content": "Keeping up with friends is faster and easier than ever.",
+"icon": "https://lh3.googleusercontent.com/ZZPdzvlpK9r_Df9C3M7j1rNRi7hhHRvPhlklJ3lfi5jk86Jd1s0Y5wcQ1QgbVaAP5Q=s360-rw",
+"big_icon": "https://lh3.googleusercontent.com/ZZPdzvlpK9r_Df9C3M7j1rNRi7hhHRvPhlklJ3lfi5jk86Jd1s0Y5wcQ1QgbVaAP5Q=s360-rw"
+}, {
+"title": "Instagram",
+"pkn": "com.instagram.android",
+"content": "Instagram is a simple way to capture and share the world’s moments.",
+"icon": "https://lh3.googleusercontent.com/aYbdIM1abwyVSUZLDKoE0CDZGRhlkpsaPOg9tNnBktUQYsXflwknnOn2Ge1Yr7rImGk=s360",
+"big_icon": "https://lh3.googleusercontent.com/aYbdIM1abwyVSUZLDKoE0CDZGRhlkpsaPOg9tNnBktUQYsXflwknnOn2Ge1Yr7rImGk=s360"
+}]
