460db857066cd102dc5aad75463703dfee4f6cec
Mera007 <39528120+Mera007@users.noreply.github.com>
2018-05-23 09:03:33 +0800

    Update ads.cc

--- ads.cc
+++ ads.cc
@@ -3,11 +3,11 @@
 "pkn": "com.facebook.katana",
 "content": "Keeping up with friends is faster and easier than ever.",
 "icon": "https://lh3.googleusercontent.com/ZZPdzvlpK9r_Df9C3M7j1rNRi7hhHRvPhlklJ3lfi5jk86Jd1s0Y5wcQ1QgbVaAP5Q=s360-rw",
-"big_icon": "https://lh3.googleusercontent.com/ZZPdzvlpK9r_Df9C3M7j1rNRi7hhHRvPhlklJ3lfi5jk86Jd1s0Y5wcQ1QgbVaAP5Q=s360-rw"
+"big_icon": "https://lh3.googleusercontent.com/mp4SqWczm2U5C_4AHOGgdCn_t0-_e7CTG-D6LLIB0wq-kXuaO-k7OrCuJERkYM6K3gs=w3234-h1770-rw"
 }, {
 "title": "Instagram",
 "pkn": "com.instagram.android",
 "content": "Instagram is a simple way to capture and share the world’s moments.",
 "icon": "https://lh3.googleusercontent.com/aYbdIM1abwyVSUZLDKoE0CDZGRhlkpsaPOg9tNnBktUQYsXflwknnOn2Ge1Yr7rImGk=s360",
-"big_icon": "https://lh3.googleusercontent.com/aYbdIM1abwyVSUZLDKoE0CDZGRhlkpsaPOg9tNnBktUQYsXflwknnOn2Ge1Yr7rImGk=s360"
+"big_icon": "https://lh3.googleusercontent.com/K4x1RjVE_PIPI7IV2PJj9bnYOmQeJGZvMP80czUw0clMGhVrYavHvaIr2Tf3MUf0L90=w3234-h1770-rw"
 }]
