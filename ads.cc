9e5d42801a7d55cb11039bf804a7724a047b7887
Mera007 <39528120+Mera007@users.noreply.github.com>
2018-09-28 16:34:33 +0800

    Update ads.cc

--- ads.cc
+++ ads.cc
@@ -1,1 +1,41 @@
-[]
+{
+	"native": [{
+			"ad": "Admob",
+			"quantity": 0,
+			"appid": "ca-app-pub-3940256099942544~3347511713",
+			"id": "ca-app-pub-3940256099942544/2247696110"
+		},
+		{
+			"ad": "Facebook",
+			"quantity": 0,
+			"appid": "",
+			"id": ""
+		}
+	],
+	"banner": [{
+			"ad": "Admob",
+			"quantity": 0,
+			"appid": "",
+			"id": ""
+		},
+		{
+			"ad": "Facebook",
+			"quantity": 0,
+			"appid": "",
+			"id": ""
+		}
+	],
+	"interstitial": [{
+			"ad": "Admob",
+			"appid": "",
+			"quantity": 0,
+			"id": ""
+		},
+		{
+			"ad": "Facebook",
+			"appid": "",
+			"quantity": 0,
+			"id": ""
+		}
+	]
+}
